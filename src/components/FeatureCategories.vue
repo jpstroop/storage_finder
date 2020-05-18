5d0b9a0a412be02947727c6c4cf4d551a54e633e
Jon Stroop <jpstroop@gmail.com>
2020-05-17 20:03:34 -0400

    show and hide long descriptions

--- src/components/FeatureCategories.vue
+++ src/components/FeatureCategories.vue
@@ -1,7 +1,16 @@
 <template>
   <ul id="feature-categories" class="col-sm-4 list-group">
     <li v-for="category in categories" :key="category.id" class="list-group-item">
-      <p class="font-weight-bold description">{{ category.description }}</p>
+      <p class="font-weight-bold description" :id="category.id+'Desc'">
+        {{ category.description }}
+        <b-icon-info-circle v-b-toggle="category.id+'Long'" class='info-icon when-closed' variant="primary"></b-icon-info-circle>
+      </p>
+      <b-collapse :id="category.id+'Long'">
+        <b-card>
+          <b-icon-x-square v-b-toggle="category.id+'Long'" variant="danger" class="close-description"></b-icon-x-square>
+          <div class="long-description" v-html="category.longDescription"></div>
+        </b-card>
+      </b-collapse>
 
       <feature-checkboxes :features="filterFeaturesByCategory(category.id)"/>
 
@@ -29,13 +38,49 @@
   }
 </script>
 
+<style>
+/* .long-description includes html that is rendered from JSON strings and
+ * scoped CSS does not work on its children.
+ */
+.long-description > * {
+  margin-bottom: .5rem;
+}
+
+.long-description {
+  margin-bottom: -.5rem;
+  font-size: smaller;
+}
+</style>
+
 <style scoped>
 .list-group-item {
   text-align: left;
 }
 
-
 .description {
   margin-bottom: .2rem;
 }
+
+.info-icon {
+  margin-left: .2rem;
+  vertical-align: -0.2em;
+}
+
+.card {
+  margin-bottom: .3rem;
+}
+
+.card-body {
+  padding: .75rem 1.25rem;
+}
+
+.close-description {
+  float: right;
+  margin: -.5rem -1rem .5rem 1rem;
+}
+
+.not-collapsed.when-closed {
+  display: none;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 new Vue({
   render: h => h(App),
