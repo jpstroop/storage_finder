a81a507c7bc13a49232789187d37d9874cab9f2a
Jon Stroop <jpstroop@gmail.com>
2020-05-17 21:18:02 -0400

    listing options; no filtering yet

--- src/components/OptionList.vue
+++ src/components/OptionList.vue
@@ -7,6 +7,9 @@
       :class="index % 2 === 0 ? 'even' : 'odd'">
       <h2>{{option.label}}</h2>
       <p>{{option.description}} <a :href="option.url">Learn More...</a></p>
+      <ul class='option-features'>
+        <li v-for="feature in option.features" :key="option+'-'+feature">{{feature}}</li>
+      </ul>
     </li>
   </ul>
 </template>
@@ -39,6 +42,23 @@ h2 {
 
 .option {
   padding: 1rem .5rem;
+}
+
+.option-features {
+  padding-left: 0;
+  margin-left: -.3rem;
+}
+
+.option-features li {
+  font-size: smaller;
+  font-weight: bold;
+  color: white;
+  background-color: dimgray;
+  display: inline;
+  margin: .3rem;
+  line-height: 1;
+  border: 1px solid dimgray;
+  padding: .3rem;
 }
 
 </style>
