6afccece2cbc85424375c49206582fb1f90acee4
Jon Stroop <jpstroop@gmail.com>
2020-05-18 22:33:16 -0400

    filtering options

--- src/components/OptionList.vue
+++ src/components/OptionList.vue
@@ -1,27 +1,59 @@
 <template>
   <ul id="option-list" class="col-sm-7">
-    <li v-for="(option, index) in options"
-      :key="option.id"
-      :id="option.id"
-      class="option"
-      :class="index % 2 === 0 ? 'even' : 'odd'">
-      <h2>{{option.label}}</h2>
-      <p>{{option.description}} <a :href="option.url">Learn More...</a></p>
-      <ul class='option-features'>
-        <li v-for="feature in option.features" :key="option+'-'+feature">{{feature}}</li>
-      </ul>
-    </li>
+    <b-collapse v-for="option in options"
+      tag="li" :visible="true"
+      :key="option.id" :id="option.id"
+      class="option">
+      <div class="option-wrapper">
+        <h2>{{option.label}}</h2>
+        <p>{{option.description}} <a :href="option.url">Learn More...</a></p>
+        <ul class='option-features'>
+          <li v-for="feature in option.features" :key="option+'-'+feature">{{feature}}</li>
+        </ul>
+      </div>
+    </b-collapse>
   </ul>
 </template>
 
 <script>
-  export default {
-    name: 'option-list',
-    props: {
-      options: Array
+export default {
+  name: 'option-list',
+  props: {
+    options: Array
+  },
+  data() {
+    return {
+      requiredFeatures: this.$store.getters.requiredFeatures
+    }
+  },
+  watch: {
+    requiredFeatures(oldRequiredFeatures, newRequiredFeatures) {
+      this.filterOptions()
+    }
+  },
+  methods: {
+    optionHasAllFeatures(option) {
+      return this.requiredFeatures.every(e => option.features.includes(e))
     },
-    methods: { }
+    optionIsCollapsed(option) {
+      return document.getElementById(option.id).style.display === 'none'
+    },
+    filterOptions() {
+      console.log(this.requiredFeatures)
+      var toHide = this.options.filter(o => !this.optionHasAllFeatures(o))
+      var toShow = this.options.filter(o => this.optionHasAllFeatures(o))
+      for (var i = 0; i < this.options.length; i++) {
+        var option = this.options[i]
+        if (toHide.includes(option) && !this.optionIsCollapsed(option)) {
+          this.$root.$emit('bv::toggle::collapse', option.id)
+        }
+        if (toShow.includes(option) && this.optionIsCollapsed(option)) {
+          this.$root.$emit('bv::toggle::collapse', option.id)
+        }
+      }
+    }
   }
+}
 </script>
 
 <style scoped>
@@ -41,6 +73,12 @@ h2 {
 }
 
 .option {
+  /* This plus option-wrapper below keeps jQuery animations smooth */
+  padding: 0;
+  border-bottom: 1px solid rgba(0,0,0,.125);
+}
+
+.option-wrapper {
   padding: 1rem .5rem;
 }
 
