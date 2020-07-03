ba24ed62514e3bb9f6ee5fb7d4daa5d3e689e965
Jon Stroop <jpstroop@gmail.com>
2020-07-03 09:17:45 -0400

    add vue config to github deploy script keep ghpages.sh from messing up master if local gh-pages branch already exits

--- ghpages.sh
+++ ghpages.sh
@@ -1,8 +1,30 @@
 #!/usr/bin/env bash
-git checkout -b gh-pages
+BRANCH="gh-pages"
+REMOTE="origin"
+VUE_CONFIG_FILE="vue.config.js"
+VUE_CONFIG="module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/storage_finder/'
+    : '/'
+}"
+
+# Returns 0 if the branch exists, 1 if not (opposite of T/F...)
+function local_branch_status {
+  git show-ref --verify --quiet refs/heads/$BRANCH
+  echo "$?"
+}
+
+if [ $(local_branch_status) == 0 ]; then
+    echo "$BRANCH branch already exists, will remove"
+    git branch -D $BRANCH
+fi
+
+git push $REMOTE --delete $BRANCH
+git checkout -b $BRANCH
+echo -e "$VUE_CONFIG" > $VUE_CONFIG_FILE
 npm run build
-git add dist
+git add -f dist $VUE_CONFIG_FILE
 git commit -m "deploy latest to github pages"
-git subtree push --prefix dist origin gh-pages
+git subtree push --prefix dist $REMOTE $BRANCH
 git checkout master
-git branch -D gh-pages
+git branch -D $BRANCH
